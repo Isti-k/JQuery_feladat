d8117a4fc5d3fa72689bdeccdd2368155e34366b
Isti-k <kiscserkes03@gmail.com>
2024-03-21 09:23:36 +0100

    gyak

---
+++ script.js
@@ -0,0 +1,29 @@
+
+window.addEventListener("load", function () {
+    elemekElerese1();
+    elemekElerese2();
+    elemekElerese3();
+});
+
+function elemekElerese1() {
+    /**Ide jön az első feladat */
+    const ELEM=document.querySelectorAll("#f1")[0]
+    console.log(ELEM.innerHTML)
+}
+function elemekElerese2() {
+    /**Ide jön az 2.  feladat */
+    const ELEM=document.querySelectorAll("#ide")[0]
+    ELEM.innerHTML = "jó reggelt!"
+}
+function elemekElerese3() {
+    /**Ide jön az 3. feladat */
+    const ELEM=document.querySelectorAll(".ide")[0]
+    ELEM.innerHTML = "jó reggelt!"
+}
+function elemekElerese4() {
+    /**Ide jön az 4. feladat */
+    const ELEM=document.querySelectorAll("#lista")[0]
+    szam=Math.random(0,30)+10
+    Math.floor(szam)
+    ELEM.innerHTML = `szöveg ${valtozonev} szöveg`
+}
